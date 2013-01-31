df9a000f870cc8fe0b6ea1c0fbbb83ab9d87266e
Lyudmil Angelov <lyudmil.i.angelov@gmail.com>
2013-02-01 00:26:15 +0100

    Package library for Meteor.

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+	summary: "Easy form generation for creating and editing database records."
+});
+
+Package.on_use(function(api) {
+	api.use('jquery', 'client');
+	api.add_files([
+		'lib/jsonform/jsv.js',
+		'lib/jsonform/jsonform.js',
+		'lib/class.js',
+		'lib/database_form.js',
+		'lib/form_model.js'
+	], 'client');
+});
