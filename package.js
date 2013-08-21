17dd74e7240d3f2ceb17952bab410d185ab397e5
DennisKvdM <dennis@atlassian.com>
2013-08-21 13:03:07 -0700

    Added underscore to api.use for accessing the _ variable

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function(api) {
 	api.use('jquery', 'client');
+	api.use('underscore', 'client');
 	if(api.export)
 		api.export(['FormModel', 'DatabaseForm']);
 	api.add_files([
